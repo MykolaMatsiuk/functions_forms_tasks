2ba71f869c1aa8512f08c1f544d0271816525e6e
Mykola Matsiuk <mykola.matsiuk@gmail.com>
2017-10-13 11:18:10 +0300

    validation changed

--- 1/process.php
+++ 1/process.php
@@ -1,7 +1,11 @@
 <?php
-require_once ("functions.php");
+require_once "functions.php";
 
-if (!isset($_POST['text1']) && !isset($_POST['text2'])) {
+if(!isset($_POST['text1']) && !isset($_POST['text2'])) {
+  die("Something wrong!");
+}
+
+if(!($text1 = $_POST['text1']) || !($text2 = $_POST['text2'])) {
   die("You must type in both text areas!");
 }
 
