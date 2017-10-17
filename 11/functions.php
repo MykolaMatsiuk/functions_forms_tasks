63c830dc5853f9750bfa9da9a637058c298f3f2e
Mykola Matsiuk <mykola.matsiuk@gmail.com>
2017-10-17 11:39:45 +0300

    solving the cyrillic input problem

--- 11/functions.php
+++ 11/functions.php
@@ -1,9 +1,19 @@
 <?php
 
-function correctText($text) {
+/*function correctText($text) {
   $text = explode(".", $text);
   foreach ($text as &$string) {
     $string = ucfirst(trim($string));
+  }
+  return implode(". ", $text);
+}*/
+
+function correctText($text) {
+  $text = explode(".", $text);
+  foreach ($text as &$string) {
+    $first = mb_substr(trim($string), 0, 1);
+    $rest = mb_substr(trim($string), 1);
+    $string = mb_strtoupper($first) . $rest;
   }
   return implode(". ", $text);
 }
